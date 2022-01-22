fa7c1ab3e7c803f0b9b1f77a9dd64b857640aa32
Wyvest <45589059+Wyvest@users.noreply.github.com>
2022-01-22 13:55:45 +0700

    1.1.2 fix regex

--- src/main/java/cc/woverflow/wcore/init/WCoreInit.java
+++ src/main/java/cc/woverflow/wcore/init/WCoreInit.java
@@ -1,5 +1,6 @@
 package cc.woverflow.wcore.init;
 
+@SuppressWarnings("unused")
 public class WCoreInit {
     public static void initialize() {
         System.out.println("W-CORE has been initialized :D");
