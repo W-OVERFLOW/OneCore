c981a731f67bdf1a61c87013ff99ada8ea19edd3
Ethan <66741050+EthanDevelops@users.noreply.github.com>
2022-01-27 22:03:19 -0500

    Update GenerateString.kt

--- src/main/kotlin/cc/woverflow/wcore/utils/GenerateString.kt
+++ src/main/kotlin/cc/woverflow/wcore/utils/GenerateString.kt
@@ -2,14 +2,11 @@ package cc.woverflow.wcore.utils
 
 import org.apache.commons.lang3.RandomStringUtils
 
-class GenerateString {
-  const val STRING_LENGTH = 50;
-  const val ALPHANUMERIC_REGEX = "[a-zA-Z0-9]+";
+const val STRING_LENGTH = 50;
+const val ALPHANUMERIC_REGEX = "[a-zA-Z0-9]+";
  
-  fun createAlphanumericString() {
-    val randomString = RandomStringUtils.randomAlphanumeric(STRING_LENGTH);
- 
-    assert(randomString.matches(Regex(ALPHANUMERIC_REGEX)));
-    return randomString;
-  }
+fun createAlphanumericString() {
+  val randomString = RandomStringUtils.randomAlphanumeric(STRING_LENGTH);
+  assert(randomString.matches(Regex(ALPHANUMERIC_REGEX)));
+  return randomString;
 }
